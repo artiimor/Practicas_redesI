0d5cc7d1662553492f9c4e272aa328646e027c1c
artiimor <amorcillop@gmail.com>
2019-10-04 07:47:59 +0000

    -itf finished (I think)

--- practica1/practica1.py
+++ practica1/practica1.py
@@ -28,18 +28,22 @@ def signal_handler(nsignal,frame):
 	logging.info('Control C pulsado')
 	if handle:
 		pcap_breakloop(handle)
-		
 
 def procesa_paquete(us,header,data):
 	global num_paquete, pdumper
-	num_paquete += 1
+	num_paquete += 1	
+	#create the dumper
+	if pdumper is None:
+		pdumper = pcap_dump_open(handle,'captura.' + args.interface + str(header.ts.tv_sec + TIME_OFFSET))
 	#TODO imprimir los N primeros bytes
 	#Escribir el tráfico al fichero de captura con el offset temporal
-	
-	#pcap_dump(pdumper, ,)
+	pcap_dump(pdumper, header, data)
+
 	
 if __name__ == "__main__":
 	global pdumper,args, handle
+
+	#Argument stuff
 	parser = argparse.ArgumentParser(description='Captura tráfico de una interfaz ( o lee de fichero) y muestra la longitud y timestamp de los 50 primeros paquetes',
 	formatter_class=RawTextHelpFormatter)
 	parser.add_argument('--file', dest='tracefile', default=False,help='Fichero pcap a abrir')
@@ -58,26 +62,22 @@ if __name__ == "__main__":
 		parser.print_help()
 		sys.exit(-1)
 
+	#create the SIGINT handler
 	signal.signal(signal.SIGINT, signal_handler)
 
 	errbuf = bytearray()
 	handle = None
 	pdumper = None
 	
-	#abrimos la interfaz para captura
-	handle = pcap_open_live(args.interface, args.nbytes, 0, 100, errbuf)
-
+	#open the interface for capture it. When its open_live we want to capture the package complete
+	handle = pcap_open_live(args.interface, args.nbytes, NO_PROMISC, ETH_FRAME_MAX, errbuf)
+	#check it went right
 	if handle is None:
 		print ("No se ha capturado nada")
 		sys.exit(-1)
 
-	#abrimos un dumper para volcar el trafico
-	pdumper = pcap_dump_open(handle,'captura.' + args.interface)
-
-	if pdumper is None:
-		print ("Creado bien el dumper")
-		sys.exit(-1)
 	
+	#loop. It is interrupted when we send SIGINT, when it reads all the packages or when there's an error
 	ret = pcap_loop(handle,50,procesa_paquete,None)
 	if ret == -1:
 		logging.error('Error al capturar un paquete')
@@ -85,10 +85,15 @@ if __name__ == "__main__":
 		logging.debug('pcap_breakloop() llamado')
 	elif ret == 0:
 		logging.debug('No mas paquetes o limite superado')
-	logging.info('{} paquetes procesados'.format(num_paquete))
-	#TODO si se ha creado un dumper cerrarlo
+	logging.info('{} paquetes procesados'.format(num_paquete)) #when it ends we show the number of packages
 
-	pcap_close(handle)
-	pcap_dump_close(pdumper)
+	#TODO si se ha creado un dumper cerrarlo
+	
+	#close descriptors
+	if handle is not None:
+		pcap_close(handle)
+
+	if pdumper is not None:
+		pcap_dump_close(pdumper)
 	
 
