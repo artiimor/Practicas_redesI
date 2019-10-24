74fd361062841b0437ba315dbf7068bf4b970331
JuanMartinP <36308570+JuanMartinP@users.noreply.github.com>
2019-10-24 11:59:54 +0200

    minor changes

--- practica2/arp.py
+++ practica2/arp.py
@@ -16,7 +16,7 @@ import time
 from threading import Lock
 from expiringdict import ExpiringDict
 
-#Semáforo global 
+#Semáforo global
 globalLock = Lock()
 #Dirección de difusión (Broadcast)
 broadcastAddr = bytes([0xFF]*6)
@@ -39,14 +39,15 @@ cache = ExpiringDict(max_len=100, max_age_seconds=10)
 
 
 
+
+'''
+Nombre: getIP
+Descripción: Esta función obtiene la dirección IP asociada a una interfaz. Esta funció NO debe ser modificada
+Argumentos:
+   -interface: nombre de la interfaz
+Retorno: Entero de 32 bits con la dirección IP de la interfaz
+'''
 def getIP(interface):
-    '''
-        Nombre: getIP
-        Descripción: Esta función obtiene la dirección IP asociada a una interfaz. Esta funció NO debe ser modificada
-        Argumentos:
-            -interface: nombre de la interfaz
-        Retorno: Entero de 32 bits con la dirección IP de la interfaz
-    '''
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     ip = fcntl.ioctl(
         s.fileno(),
@@ -56,13 +57,15 @@ def getIP(interface):
     s.close()
     return struct.unpack('!I',ip)[0]
 
+
+'''
+Nombre: printCache
+Descripción: Esta función imprime la caché ARP
+Argumentos: Ninguno
+Retorno: Ninguno
+'''
 def printCache():
-    '''
-        Nombre: printCache
-        Descripción: Esta función imprime la caché ARP
-        Argumentos: Ninguno
-        Retorno: Ninguno
-    '''
+
     print('{:>12}\t\t{:>12}'.format('IP','MAC'))
     with cacheLock:
         for k in cache:
@@ -71,24 +74,26 @@ def printCache():
 
 
 
+
+'''
+Nombre: processARPRequest
+Decripción: Esta función procesa una petición ARP. Esta función debe realizar, al menos, las siguientes tareas:
+    -Extraer la MAC origen contenida en la petición ARP
+    -Si la MAC origen de la trama ARP no es la misma que la recibida del nivel Ethernet retornar
+    -Extraer la IP origen contenida en la petición ARP
+    -Extraer la IP destino contenida en la petición ARP
+    -Comprobar si la IP destino de la petición ARP es la propia IP:
+        -Si no es la propia IP retornar
+        -Si es la propia IP:
+            -Construir una respuesta ARP llamando a createARPReply (descripción más adelante)
+            -Enviar la respuesta ARP usando el nivel Ethernet (sendEthernetFrame)
+Argumentos:
+    -data: bytearray con el contenido de la trama ARP (después de la cabecera común)
+    -MAC: dirección MAC origen extraída por el nivel Ethernet
+Retorno: Ninguno
+'''
 def processARPRequest(data, MAC):
-    '''
-        Nombre: processARPRequest
-        Decripción: Esta función procesa una petición ARP. Esta función debe realizar, al menos, las siguientes tareas:
-            -Extraer la MAC origen contenida en la petición ARP
-            -Si la MAC origen de la trama ARP no es la misma que la recibida del nivel Ethernet retornar
-            -Extraer la IP origen contenida en la petición ARP
-            -Extraer la IP destino contenida en la petición ARP
-            -Comprobar si la IP destino de la petición ARP es la propia IP:
-                -Si no es la propia IP retornar
-                -Si es la propia IP:
-                    -Construir una respuesta ARP llamando a createARPReply (descripción más adelante)
-                    -Enviar la respuesta ARP usando el nivel Ethernet (sendEthernetFrame)
-        Argumentos:
-            -data: bytearray con el contenido de la trama ARP (después de la cabecera común)
-            -MAC: dirección MAC origen extraída por el nivel Ethernet
-        Retorno: Ninguno
-    '''
+
     logging.debug('Función no implementada')
 
     # TODO del byte 0 al 5 es la direccion MAC de origen suponiendo que este sea el primer campo de la parte no comun
@@ -114,29 +119,30 @@ def processARPRequest(data, MAC):
     return
 
 
+'''
+Nombre: processARPReply
+Decripción: Esta función procesa una respuesta ARP. Esta función debe realizar, al menos, las siguientes tareas:
+    -Extraer la MAC origen contenida en la petición ARP
+    -Si la MAC origen de la trama ARP no es la misma que la recibida del nivel Ethernet retornar
+    -Extraer la IP origen contenida en la petición ARP
+    -Extraer la MAC destino contenida en la petición ARP
+    -Extraer la IP destino contenida en la petición ARP
+    -Comprobar si la IP destino de la petición ARP es la propia IP:
+        -Si no es la propia IP retornar
+        -Si es la propia IP:
+            -Comprobar si la IP origen se corresponde con la solicitada (requestedIP). Si no se corresponde retornar
+            -Copiar la MAC origen a la variable global resolvedMAC
+            -Añadir a la caché ARP la asociación MAC/IP.
+            -Cambiar el valor de la variable awaitingResponse a False
+            -Cambiar el valor de la variable requestedIP a None
+Las variables globales (requestedIP, awaitingResponse y resolvedMAC) son accedidas concurrentemente por la función ARPResolution y deben ser protegidas mediante un Lock.
+Argumentos:
+    -data: bytearray con el contenido de la trama ARP (después de la cabecera común)
+    -MAC: dirección MAC origen extraída por el nivel Ethernet
+Retorno: Ninguno
+'''
 def processARPReply(data,MAC):
-    '''
-        Nombre: processARPReply
-        Decripción: Esta función procesa una respuesta ARP. Esta función debe realizar, al menos, las siguientes tareas:
-            -Extraer la MAC origen contenida en la petición ARP
-            -Si la MAC origen de la trama ARP no es la misma que la recibida del nivel Ethernet retornar
-            -Extraer la IP origen contenida en la petición ARP
-            -Extraer la MAC destino contenida en la petición ARP
-            -Extraer la IP destino contenida en la petición ARP
-            -Comprobar si la IP destino de la petición ARP es la propia IP:
-                -Si no es la propia IP retornar
-                -Si es la propia IP:
-                    -Comprobar si la IP origen se corresponde con la solicitada (requestedIP). Si no se corresponde retornar
-                    -Copiar la MAC origen a la variable global resolvedMAC
-                    -Añadir a la caché ARP la asociación MAC/IP.
-                    -Cambiar el valor de la variable awaitingResponse a False
-                    -Cambiar el valor de la variable requestedIP a None
-        Las variables globales (requestedIP, awaitingResponse y resolvedMAC) son accedidas concurrentemente por la función ARPResolution y deben ser protegidas mediante un Lock.
-        Argumentos:
-            -data: bytearray con el contenido de la trama ARP (después de la cabecera común)
-            -MAC: dirección MAC origen extraída por el nivel Ethernet
-        Retorno: Ninguno
-    '''
+
     global requestedIP,resolvedMAC,awaitingResponse,cache
     logging.debug('Función no implentada')
 
@@ -165,37 +171,38 @@ def processARPReply(data,MAC):
 
     # TODO esto hay que protegerlo con lock()
     resolvedMAC = mac_origen
-    # aniadimos el par ip/mac , son las de origen porque en una arp reply las direcciones de origen son las de destino en la request
+    # aniadimos el par ip/mac , son las de origen porque el arp reply tiene como origen las de destino del arp request
     cache = {ip_origen: mac_origen}
     awaitingResponse = False
     requestedIP = None
 
     return
 
+'''
+Nombre: createARPRequest
+Descripción: Esta función construye una petición ARP y devuelve la trama con el contenido.
+Argumentos:
+    -ip: dirección a resolver
+Retorno: Bytes con el contenido de la trama de petición ARP
+'''
 def createARPRequest(ip):
-    '''
-        Nombre: createARPRequest
-        Descripción: Esta función construye una petición ARP y devuelve la trama con el contenido.
-        Argumentos: 
-            -ip: dirección a resolver 
-        Retorno: Bytes con el contenido de la trama de petición ARP
-    '''
+
     global myMAC,myIP
     frame = bytes()
     logging.debug('Función no implementada')
     #TODO implementar aqui
     return frame
 
-    
+
 def createARPReply(IP,MAC):
     '''
-        Nombre: createARPReply
-        Descripción: Esta función construye una respuesta ARP y devuelve la trama con el contenido.
-        Argumentos: 
-            -IP: dirección IP a la que contestar
-            -MAC: dirección MAC a la que contestar
-        Retorno: Bytes con el contenido de la trama de petición ARP
-    '''
+Nombre: createARPReply
+Descripción: Esta función construye una respuesta ARP y devuelve la trama con el contenido.
+Argumentos:
+    -IP: dirección IP a la que contestar
+    -MAC: dirección MAC a la que contestar
+Retorno: Bytes con el contenido de la trama de petición ARP
+'''
     global myMAC,myIP
     frame = bytes()
     logging.debug('Función no implementada')
@@ -204,62 +211,67 @@ def createARPReply(IP,MAC):
 
 
 def process_arp_frame(us,header,data,srcMac):
-    '''
-        Nombre: process_arp_frame
-        Descripción: Esta función procesa las tramas ARP. 
-            Se ejecutará por cada trama Ethenet que se reciba con Ethertype 0x0806 (si ha sido registrada en initARP). 
-            Esta función debe realizar, al menos, las siguientes tareas:
-                -Extraer la cabecera común de ARP (6 primeros bytes) y comprobar que es correcta
-                -Extraer el campo opcode
-                -Si opcode es 0x0001 (Request) llamar a processARPRequest (ver descripción más adelante)
-                -Si opcode es 0x0002 (Reply) llamar a processARPReply (ver descripción más adelante)
-                -Si es otro opcode retornar de la función
-                -En caso de que no exista retornar
-        Argumentos:
-            -us: Datos de usuario pasados desde la llamada de pcap_loop. En nuestro caso será None
-            -header: cabecera pcap_pktheader
-            -data: array de bytes con el contenido de la trama ARP
-            -srcMac: MAC origen de la trama Ethernet que se ha recibido
-        Retorno: Ninguno
-    '''
+'''
+Nombre: process_arp_frame
+Descripción: Esta función procesa las tramas ARP.
+Se ejecutará por cada trama Ethenet que se reciba con Ethertype 0x0806 (si ha sido registrada en initARP).
+Esta función debe realizar, al menos, las siguientes tareas:
+    -Extraer la cabecera común de ARP (6 primeros bytes) y comprobar que es correcta
+    -Extraer el campo opcode
+    -Si opcode es 0x0001 (Request) llamar a processARPRequest (ver descripción más adelante)
+    -Si opcode es 0x0002 (Reply) llamar a processARPReply (ver descripción más adelante)
+    -Si es otro opcode retornar de la función
+    -En caso de que no exista retornar
+Argumentos:
+    -us: Datos de usuario pasados desde la llamada de pcap_loop. En nuestro caso será None
+    -header: cabecera pcap_pktheader
+    -data: array de bytes con el contenido de la trama ARP
+    -srcMac: MAC origen de la trama Ethernet que se ha recibido
+Retorno: Ninguno
+'''
     logging.debug('Función no implementada')
     #TODO implementar aquí
 
 
 
+
+'''
+Nombre: initARP
+Descripción: Esta función construirá inicializará el nivel ARP. Esta función debe realizar, al menos, las siguientes tareas:
+    -Registrar la función del callback process_arp_frame con el Ethertype 0x0806
+    -Obtener y almacenar la dirección MAC e IP asociadas a la interfaz especificada
+    -Realizar una petición ARP gratuita y comprobar si la IP propia ya está asignada. En caso positivo se debe devolver error.
+    -Marcar la variable de nivel ARP inicializado a True
+'''
 def initARP(interface):
-    '''
-        Nombre: initARP
-        Descripción: Esta función construirá inicializará el nivel ARP. Esta función debe realizar, al menos, las siguientes tareas:
-            -Registrar la función del callback process_arp_frame con el Ethertype 0x0806
-            -Obtener y almacenar la dirección MAC e IP asociadas a la interfaz especificada
-            -Realizar una petición ARP gratuita y comprobar si la IP propia ya está asignada. En caso positivo se debe devolver error.
-            -Marcar la variable de nivel ARP inicializado a True
-    '''
+
     global myIP,myMAC,arpInitialized
     logging.debug('Función no implementada')
     #TODO implementar aquí
     return 0
 
+
+
+'''
+Nombre: ARPResolution
+Descripción: Esta función intenta realizar una resolución ARP para una IP dada y devuelve la dirección MAC asociada a dicha IP
+o None en caso de que no haya recibido respuesta. Esta función debe realizar, al menos, las siguientes tareas:
+    -Comprobar si la IP solicitada existe en la caché:
+    -Si está en caché devolver la información de la caché
+    -Si no está en la caché:
+        -Construir una petición ARP llamando a la función createARPRequest (descripción más adelante)
+        -Enviar dicha petición
+        -Comprobar si se ha recibido respuesta o no:
+            -Si no se ha recibido respuesta reenviar la petición hasta un máximo de 3 veces. Si no se recibe respuesta devolver None
+            -Si se ha recibido respuesta devolver la dirección MAC
+Esta función necesitará comunicarse con el la función de recepción (para comprobar si hay respuesta y la respuesta en sí) mediante 3 variables globales:
+    -awaitingResponse: indica si está True que se espera respuesta. Si está a False quiere decir que se ha recibido respuesta
+    -requestedIP: contiene la IP por la que se está preguntando
+    -resolvedMAC: contiene la dirección MAC resuelta (en caso de que awaitingResponse) sea False.
+Como estas variables globales se leen y escriben concurrentemente deben ser protegidas con un Lock
+'''
 def ARPResolution(ip):
-    '''
-        Nombre: ARPResolution
-        Descripción: Esta función intenta realizar una resolución ARP para una IP dada y devuelve la dirección MAC asociada a dicha IP 
-            o None en caso de que no haya recibido respuesta. Esta función debe realizar, al menos, las siguientes tareas:
-                -Comprobar si la IP solicitada existe en la caché:
-                -Si está en caché devolver la información de la caché
-                -Si no está en la caché:
-                    -Construir una petición ARP llamando a la función createARPRequest (descripción más adelante)
-                    -Enviar dicha petición
-                    -Comprobar si se ha recibido respuesta o no:
-                        -Si no se ha recibido respuesta reenviar la petición hasta un máximo de 3 veces. Si no se recibe respuesta devolver None
-                        -Si se ha recibido respuesta devolver la dirección MAC
-            Esta función necesitará comunicarse con el la función de recepción (para comprobar si hay respuesta y la respuesta en sí) mediante 3 variables globales:
-                -awaitingResponse: indica si está True que se espera respuesta. Si está a False quiere decir que se ha recibido respuesta
-                -requestedIP: contiene la IP por la que se está preguntando
-                -resolvedMAC: contiene la dirección MAC resuelta (en caso de que awaitingResponse) sea False.
-            Como estas variables globales se leen y escriben concurrentemente deben ser protegidas con un Lock
-    '''
+
     global requestedIP,awaitingResponse,resolvedMAC
     logging.debug('Función no implementada')
     #TODO implementar aquí
